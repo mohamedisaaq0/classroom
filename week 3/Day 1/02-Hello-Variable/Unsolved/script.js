c1e4abe48e80d3f6c81e9053e5b2a9c6842a502f
mohamed hassan <mohamedisaaq0@gmail.com>
2022-04-12 20:38:14 +0100

    variable excerise complete

--- week 3/Day 1/02-Hello-Variable/Unsolved/script.js
+++ week 3/Day 1/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,9 @@
 // Write Your JavaScript Code Here
+
+var personName = "mohamed hassan";
+var pet = 5;
+var funFact = "I play football";
+
+console.log("I am ", personName);
+console.log("I have ", pet, " pets");
+console.log("Fun Fact: ", funFact);
