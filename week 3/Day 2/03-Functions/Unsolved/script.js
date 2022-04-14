aba075dac06271cd64dce00c546a9f4a89c27013
mohamed hassan <mohamedisaaq0@gmail.com>
2022-04-14 21:33:23 +0100

    completed the function task

---
+++ week 3/Day 2/03-Functions/Unsolved/script.js
@@ -0,0 +1,19 @@
+// Write Your Code Below
+
+
+
+
+function compareValue(x,y) {
+    if (x===y) {
+    return "they are equal in type and value";
+}   else if (x==y) {
+        return "they are equal in value"
+    }
+    else {
+        return "they are not equals"
+    }
+}
+
+console.log(compareValue(5))
+
+
