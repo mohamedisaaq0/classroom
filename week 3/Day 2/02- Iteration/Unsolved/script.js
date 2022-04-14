67f18235d7ca7d3fd4dd98b475050df9497fc00e
mohamed hassan <mohamedisaaq0@gmail.com>
2022-04-14 19:58:25 +0100

    iteration exxcerise complete

---
+++ week 3/Day 2/02- Iteration/Unsolved/script.js
@@ -0,0 +1,12 @@
+//WRITE YOUR CODE BELOW
+
+
+var students = ["student1", "student2", "student3", "student4"]
+
+console.log(students.length)
+
+
+for (let i = 0; i < students.length; i++) {
+     console.log("Great to see you,", students[i] + "!");
+    
+}
